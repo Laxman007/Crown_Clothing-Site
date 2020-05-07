de4f844d91aa31d2d1fd2f40399951017fc13184
laxman <laxmangoel209@gmail.com>
2020-05-07 22:01:05 +0530

    Adding my homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Homepage from './homepage.component';
 
 function App() {
   return (
     <div className="App">
-   		<p>jshffh</p> 
+      <Homepage />
     </div>
-  );
+  );  
 }
 
-export default App;
+export default App; 
