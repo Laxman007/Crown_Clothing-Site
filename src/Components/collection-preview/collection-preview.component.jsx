972059f4b9bf9efd42669f7d1cc27e8c34526fd6
laxman <laxmangoel209@gmail.com>
2020-05-09 17:34:21 +0530

    Adding one more component

--- src/Components/collection-preview/collection-preview.component.jsx
+++ src/Components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './collection-preview.styles.scss';
-
+import CollectionItem from '../collection-item/collection-item.component';
 const CollectionPreview=({title,items})=>(
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((item,idx)=> idx < 4).map((item)=>(
-                    <div key={item.id} >{item.name}</div>
+                items.filter((item,idx)=> idx < 4).map(({id,...otheritemProps})=>(
+                    <CollectionItem key={id} {...otheritemProps} />
                 ))
             }
         </div>
