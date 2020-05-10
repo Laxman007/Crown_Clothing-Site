6c2105c97e95aea810b645cc3603f2a4d90ae809
laxman <laxmangoel209@gmail.com>
2020-05-10 15:50:03 +0530

    Adding sign-in page without styles yet..

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.scss';
 import {Switch,Route} from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './Components/header/header.component.jsx'
-
+import Header from './Components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in_and_sign-up/sign-in_and_sign-up.component';
 
 function App() { 
   return (
@@ -13,6 +13,7 @@ function App() {
       <Switch>
       <Route exact path='/' component={Homepage} />
       <Route  path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
       
     </div>
---
+++ src/pages/sign-in_and_sign-up/sign-in_and_sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in_and_sign-up.styles.scss';
+import SignIn from '../../Components/sign-in/sign-in.component';
+const SignInAndSignUpPage=()=>(
+
+    <div className=''>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUpPage;
