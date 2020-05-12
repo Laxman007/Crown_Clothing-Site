6fbdd51334fb004125ade8888f91e17802b563fd
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-05-12 21:30:24 +0530

    Doing some state changes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './Components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in_and_sign-up/sign-in_and_sign-up.component';
+import {auth} from './firebase/firebase.utils';
 
 
-function App() { 
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route  path='/shop' component={ShopPage} />
-      <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-      
-    </div>
-  );  
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      currentUser:null
+    }
+  }
+  unSubscribeFromAuth=null;
+  componentDidMount(){
+   this.unSubscribeFromAuth = auth.onAuthStateChanged(user=>{
+      this.setState({currentUser:user} );
+      console.log(user);
+    });
+  }
+  componentWillUnmount()
+  {
+    this.unSubscribeFromAuth();
+  }
+  render(){ 
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route  path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );  
+  }
 }
 
 export default App; 
