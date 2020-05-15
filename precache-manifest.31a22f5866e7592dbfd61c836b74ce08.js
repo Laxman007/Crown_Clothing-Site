a701d8f2aa09b7812b9b9035ac0a5716002949e1
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-05-15 20:37:58 +0530

    Updates

---
+++ precache-manifest.31a22f5866e7592dbfd61c836b74ce08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a730395882d5f5662365a6f83f87a5",
+    "url": "/Crown-Clothing-Site/index.html"
+  },
+  {
+    "revision": "8e110468b7c106864fb6",
+    "url": "/Crown-Clothing-Site/static/css/main.6edbb5d0.chunk.css"
+  },
+  {
+    "revision": "a6f943f9431223e5792e",
+    "url": "/Crown-Clothing-Site/static/js/2.f9aa8020.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/Crown-Clothing-Site/static/js/2.f9aa8020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e110468b7c106864fb6",
+    "url": "/Crown-Clothing-Site/static/js/main.c91c8bdf.chunk.js"
+  },
+  {
+    "revision": "11ea04b31d60e17382c7",
+    "url": "/Crown-Clothing-Site/static/js/runtime-main.4614b9c6.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Crown-Clothing-Site/static/media/crown.ed28accb.svg"
+  }
+]);
