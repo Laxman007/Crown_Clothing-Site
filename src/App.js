64f1f57bb47258059115d7dc8ade20f92fb25741
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-05-16 22:31:05 +0530

    Adding firestore to store data into project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './Components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in_and_sign-up/sign-in_and_sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -17,9 +17,21 @@ class App extends React.Component {
   }
   unsubscribeFromAuth=null;
   componentDidMount(){
-   this.unsubscribeFromAuth = auth.onAuthStateChanged(user=>{
-      this.setState({currentUser:user} );
-    
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
+      if(userAuth){
+        const userRef=await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot=>{
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      }
+      else{
+        this.setState({currentUser:userAuth});
+      }
     });
   } 
   componentWillUnmount()
