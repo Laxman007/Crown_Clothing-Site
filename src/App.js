01dc24f321a4b426484ae366f4b6be4e0b0c50cc
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-05-19 11:11:15 +0530

    Adding some changes

--- src/App.js
+++ src/App.js
@@ -26,13 +26,11 @@ class App extends React.Component {
               id:snapShot.id,
               ...snapShot.data()
             }
-          });
-          console.log(this.state);
+          }); 
         });
       }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-      else{ 
+      
         this.setState({currentUser:userAuth});
-      }
     });
   } 
   componentWillUnmount()
