397fea99a86be5e989cb615be94f03852a135dd1
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-05-21 22:02:54 +0530

    Remove state in app.js and use redux in place of it

--- src/App.js
+++ src/App.js
@@ -6,31 +6,26 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './Components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in_and_sign-up/sign-in_and_sign-up.component';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
-
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      currentUser:null
-    }
-  }
+
   unsubscribeFromAuth=null;
   componentDidMount(){
+    const {setCurrentUser}=this.props; 
    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
       if(userAuth){
         const userRef=await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot=>{
-          this.setState({
-            currentUser:{
+          setCurrentUser({
               id:snapShot.id,
               ...snapShot.data()
-            }
+            });
           }); 
-        });
-      }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
       
-        this.setState({currentUser:userAuth});
+        setCurrentUser(userAuth);
     });
   } 
   componentWillUnmount()
@@ -40,7 +35,7 @@ class App extends React.Component {
   render(){ 
     return (
       <div className="App">
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
         <Route exact path='/' component={Homepage} />
         <Route  path='/shop' component={ShopPage} />
@@ -51,4 +46,8 @@ class App extends React.Component {
   }
 }
 
-export default App; 
+const mapDispatchToProps=dispatch=>({
+  setCurrentUser:user=>dispatch(setCurrentUser(user))
+});
+
+export default connect(null,mapDispatchToProps)(App); 
