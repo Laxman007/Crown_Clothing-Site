1ff88c0c50b35047ca9d5c2a6bf739e6417f9ef8
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-05-31 22:01:23 +0530

    Adding stripe payment feature into the application

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,8 @@ import {selectCartItems,selectCartTotal} from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
 import CheckoutItem from '../../Components/checkout-item/checkout-item.component';
 
+import StripeCheckoutButton from '../../Components/stripe-button/stripe-button.component';
+
 const CheckoutPage=({cartItems,total})=>(
     <div className='checkout-page'>
         <div className='checkout-header'>
@@ -34,6 +36,12 @@ const CheckoutPage=({cartItems,total})=>(
         <div className='total'>
             <span>TOTAL : ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp:01/23 - CVV:123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
